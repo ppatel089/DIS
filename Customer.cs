636b43d69ff753915f02e04637ec7d1e77af1310
ppatel089 <49743449+ppatel089@users.noreply.github.com>
2019-09-24 23:09:51 -0400

    In-class # 3

---
+++ Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP_Introduction
+{
+    class Customer : Person
+    {
+        public string studentNumber;
+        public Person Advisor;
+
+        /// <summary>
+        /// Default constructor attempt 1
+        /// </summary>
+        public Customer()
+        {
+            CustomerNumber = "U1234";
+            FirstName = "Pankaj";
+        }
+
+        /// <summary>
+        /// Default constructor that uses inheritance
+        /// </summary>
+        //public Student() : base()
+        //{
+        //  studentNumber = "U1234";
+        //}
+    }
+}
